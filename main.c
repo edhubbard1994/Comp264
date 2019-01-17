ec35378ada915acdb83371305f9d4dbc19e8ec1b
Edward Hubbard <edwardhubbard@Edwards-MacBook-Air.local>
2019-01-17 13:19:05 -0600

    first

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  EdwardHubbardHelloWorld
+//
+//  Created by Edward Hubbard on 1/17/19.
+//  Copyright © 2019 Edward Hubbard. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+     char* hello = "Hello World\n";
+    printf("%s",hello);
+    return 0;
+}
