241c6a485e06830775510795c4093b49e07378e9
Hubbard <edhubbard1994@gmail.com>
2019-03-19 20:23:45 -0500

    bad

--- hw4/hwfour.s
+++ hw4/hwfour.s
@@ -1,65 +1,60 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 10, 14
-	.globl	_decode2                ## -- Begin function decode2
-	.p2align	4, 0x90
-_decode2:                               ## @decode2
+	.file	"hwfour.c"
+	.text
+	.globl	decode2
+	.type	decode2, @function
+decode2:
+.LFB2:
 	.cfi_startproc
-## %bb.0:
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
+	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	movq	%rdi, -8(%rbp)
-	movq	%rsi, -16(%rbp)
-	movq	%rdx, -24(%rbp)
-	movq	-8(%rbp), %rdx
-	movq	-16(%rbp), %rsi
-	subq	%rdx, %rsi
-	movq	%rsi, -16(%rbp)
-	movq	-16(%rbp), %rdx
-	imulq	-24(%rbp), %rdx
-	movq	%rdx, -24(%rbp)
-	movq	-24(%rbp), %rdx
-	movq	%rdx, -32(%rbp)
-	movq	-32(%rbp), %rdx
-	shlq	$15, %rdx
-	movq	%rdx, -32(%rbp)
-	movq	-32(%rbp), %rdx
-	sarq	$31, %rdx
-	movq	%rdx, -32(%rbp)
-	movq	-24(%rbp), %rdx
-	xorq	-32(%rbp), %rdx
-	movq	%rdx, -32(%rbp)
-	movq	-32(%rbp), %rax
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	%rdx, -40(%rbp)
+	movq	-24(%rbp), %rax
+	subq	%rax, -32(%rbp)
+	movq	-40(%rbp), %rax
+	imulq	-32(%rbp), %rax
+	movq	%rax, -40(%rbp)
+	movq	-40(%rbp), %rax
+	movq	%rax, -8(%rbp)
+	salq	$15, -8(%rbp)
+	sarq	$31, -8(%rbp)
+	movq	-40(%rbp), %rax
+	xorq	%rax, -8(%rbp)
+	movq	-8(%rbp), %rax
 	popq	%rbp
-	retq
+	.cfi_def_cfa 7, 8
+	ret
 	.cfi_endproc
-                                        ## -- End function
-	.globl	_main                   ## -- Begin function main
-	.p2align	4, 0x90
-_main:                                  ## @main
+.LFE2:
+	.size	decode2, .-decode2
+	.globl	main
+	.type	main, @function
+main:
+.LFB3:
 	.cfi_startproc
-## %bb.0:
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
+	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	subq	$48, %rsp
-	movl	$0, -4(%rbp)
-	movq	$2, -32(%rbp)
-	movq	-16(%rbp), %rdi
-	movq	-24(%rbp), %rsi
-	movq	-32(%rbp), %rdx
-	callq	_decode2
-	xorl	%ecx, %ecx
-	movq	%rax, -40(%rbp)         ## 8-byte Spill
-	movl	%ecx, %eax
-	addq	$48, %rsp
-	popq	%rbp
-	retq
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	$2, -24(%rbp)
+	movq	-24(%rbp), %rdx
+	movq	-8(%rbp), %rcx
+	movq	-16(%rbp), %rax
+	movq	%rcx, %rsi
+	movq	%rax, %rdi
+	call	decode2
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
 	.cfi_endproc
-                                        ## -- End function
-
-.subsections_via_symbols
+.LFE3:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
